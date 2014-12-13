3dc6f0e76ce2a47a184f0abcbd003cb781d3f0b3
resheto@gmail.com <resheto@gmail.com@9037874a-866b-ba1d-13bd-c2d32d373a6c>
2014-12-13 17:49:38 +0000

    init

---
+++ trunk/src/main/java/com/googlecode/scheme2ddl/db2/dao/ConnectionDao.java
@@ -0,0 +1,20 @@
+package com.googlecode.scheme2ddl.db2.dao;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Check some properties of connection
+ *
+ * @author A_Reshetnikov
+ * @since Date: 23.07.2013
+ */
+public interface ConnectionDao {
+
+    boolean isConnectionAvailable();
+
+    boolean hasSelectCatalogRole();
+
+    List<String> findAvailableSchemas();
+}
